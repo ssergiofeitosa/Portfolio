8295bffcd40775b1590e65c67dff2d3925177884
Francisco Sérgio Feitosa <156242600+ssergiofeitosa@users.noreply.github.com>
2025-02-12 20:53:45 -0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+      <footer className="bg-white dark:bg-gray-950">
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 flex justify-center">
+          <span className="text-sm text-gray-500 dark:text-gray-400 text-center">
+            © 2025{' '}
+            <a href="" className="hover:underline">
+              F. Sérgio Feitosa
+            </a>
+            . Todos os direitos reservados.
+          </span>
+        </div>
+      </footer>
+    );
+}

cfcef5191782a090f308491c8a68af907cdfa764
Francisco Sérgio Feitosa <156242600+ssergiofeitosa@users.noreply.github.com>
2025-02-12 20:50:40 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import Skills from "./components/Skills";
 import Hero from './components/Hero';
 // import Form from './components/Form';
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
 
-      <div className="min-h-fit max-w-full items-center w-full h-screen bg-gradient-to-r from-gray-950 to-zinc-900 flex flex-col">
+      <div className="min-h-screen flex flex-col justify-between bg-gray-950 items-center">
         <Header />
         <Hero></Hero>
         <Skills />
         <Contact></Contact>
+        <Footer></Footer>
       </div>
 
       

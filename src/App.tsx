608816d8967f1a766411b414828efb16522b13aa
ssergiofeitosa <fsfsegundo@hotmail.com>
2025-03-11 22:39:07 -0300

    chore:removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,9 @@ import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
-import Carousel from './components/carousel/Carousel';
 import Footer from "./components/Footer";
 import Swiper from "./components/swiper/Swiper";
 function App() {
-  const projects = [
-    {
-      id: "item-1",
-      image: "/burguermania.png",
-      title: "Burguer Mania",
-      technologies: ["Angular", "TypeScript", "CSS"],
-      description: "Um aplicativo de delivery de hambúrgueres desenvolvido com Angular e TypeScript.",
-      link:"https://github.com/ssergiofeitosa/Projeto-Restic---Burguer-Mania.git",
-      site:""
-    },
-    {
-      id: "item-2",
-      image: "/quiz.png",
-      title: "Quiz App de História",
-      technologies: ["JavaScript", "TypeScript", "CSS"],
-      description: "Um quiz interativo sobre história mundial, desenvolvido com JavaScript e TypeScript. O aplicativo inclui perguntas de múltipla escolha, um sistema de pontuação e feedback instantâneo.",
-      link:"https://github.com/ssergiofeitosa/Projeto-Restic---Quiz.git",
-      site:"https://projeto-restic-quiz-historia.vercel.app"
-    },
-    {
-      id: "item-3",
-      image: "/googleformclone.png",
-      title: "Google Form Clone",
-      technologies: ["React", "TypeScript", "TailwindCSS"],
-      description: "Um clone do Google Forms desenvolvido com React e TailwindCSS. ",
-      link:"https://github.com/ssergiofeitosa/Google-Form-Clone-React.git",
-      site:""
-    },
-  ];
 
   return (
     <>

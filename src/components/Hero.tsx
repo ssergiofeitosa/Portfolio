f16705bf7043403a77bc9eba843d3616d363aa0b
Francisco Sérgio Feitosa <fsfsegundo@hotmail.com>
2025-04-26 15:05:44 -0300

    feat:changed Hero section layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,181 +1,189 @@
-import { motion } from "framer-motion";
-import "animate.css";
-import {
-  GithubLogo,
-  LinkedinLogo,
-  InstagramLogo,
-  EnvelopeSimple,
-  ArrowDown, Download
-} from "phosphor-react";
+import { motion } from "framer-motion"
+import "animate.css"
+import { GithubLogo, LinkedinLogo, InstagramLogo, EnvelopeSimple, ArrowDown, Download } from "phosphor-react"
 
 const Hero = () => {
+  
+
+ 
+
   const scrollToSection = () => {
-    const section = document.querySelector("#projects");
+    const section = document.querySelector("#projects")
     if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
+      section.scrollIntoView({ behavior: "smooth" })
     }
-  };
+  }
+
   return (
     <section
       id="home"
       className="w-full min-h-screen flex flex-col justify-center items-center relative overflow-hidden snap-start"
     >
-      <div className="absolute top-0 left-0 w-full h-40 bg-gradient-to-b from-neutral-900 to-transparent z-0"></div>
 
-      {/* gradiente de fundo */}
-      <div className="absolute inset-0 bg-gradient-to-b from-neutral-950 via-neutral-900 to-neutral-950 z-[-1]"></div>
 
-      {/* animação de fundo (pontos) */}
-      <div className="absolute inset-0 z-[-1]">
-        <div className="absolute w-full h-full opacity-20 bg-[radial-gradient(circle_at_center,_rgba(255,255,255,0.1)_1px,_transparent_1px)] bg-[length:20px_20px]"></div>
-      </div>
-
-      <div className="container mx-auto px-4 flex flex-col items-center text-center gap-6 mt-16">
-        {/* borda animada na foto de perfil*/}
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="relative"
-        >
-          <div className="absolute inset-0 rounded-full bg-gradient-to-r from-rose-500 to-rose-700 blur-md opacity-70 animate-pulse"></div>
-          <img
-            className="relative w-37 h-37 sm:w-43 sm:h-43 object-cover rounded-full border-2 border-rose-500 p-1 bg-neutral-900"
-            src="/eu.jpeg"
-            alt="Sérgio Feitosa"
-            // width={120}
-            // height={120}
-          />
-        </motion.div>
-
-        {/* Name with animated brackets */}
-        <motion.h1
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
-          className="sm:text-5xl text-4xl font-bold text-white"
-        >
-          <motion.span
-            initial={{ x: -20, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ delay: 0.4, duration: 0.5 }}
-            className="text-rose-500 inline-block"
+      <div className="container mx-auto px-4 flex flex-col items-center text-center md:text-left gap-6 mt-16">
+        {/* container pai*/}
+        <div className="w-full flex flex-col md:flex-row md:items-center md:justify-between gap-8 md:gap-12">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="relative md:w-1/3 flex justify-center"
           >
-            {"<"}
-          </motion.span>
-          <span className="relative">
-            Sérgio Feitosa
-            <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-gradient-to-r from-transparent via-rose-500 to-transparent"></span>
-          </span>
-          <motion.span
-            initial={{ x: 20, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ delay: 0.4, duration: 0.5 }}
-            className="text-rose-500 inline-block"
-          >
-            {"/>"}
-          </motion.span>
-        </motion.h1>
+            <div className="absolute top-1/2 left-1/2 w-44 h-44 md:w-64 md:h-64 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gradient-to-r from-rose-700 to-red-700 blur-sm  animate-pulse "></div>
+            <img
+              className="relative w-40 h-40 md:w-64 md:h-64 object-cover rounded-full  border-2 border-rose-600 p-1 bg-neutral-900"
+              src="/eu.jpeg"
+              alt="Sérgio Feitosa"
+            />
+          </motion.div>
 
-        {/* animacao do titulo */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.6, duration: 0.5 }}
-          className="relative"
-        >
-          <h2 className="sm:text-3xl text-2xl font-medium text-white">
-            Desenvolvedor Web
-          </h2>
-          <div className="absolute -right-4 top-0 h-full w-1 bg-rose-500 animate-blink"></div>
-        </motion.div>
-
-        {/* animacao da descricao */}
-        <motion.p
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.8, duration: 0.5 }}
-          className="sm:text-3xl max-w-4xl text-center   font-light text-gray-300"
-        >
-          Graduando em{" "}
-          <span className="italic text-white font-medium relative inline-block">
-            Engenharia de Computação
-            <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-rose-500/50"></span>
-          </span>{" "}
-          na Universidade Federal do Vale do São Francisco
-        </motion.p>
-
-        {/* icones c hover */}
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 1, duration: 0.5 }}
-          className="flex gap-5 mt-4"
-        >
-          {[
-            {
-              icon: <GithubLogo size={32} weight="light" />,
-              href: "https://github.com/ssergiofeitosa",
-              label: "GitHub",
-            },
-            {
-              icon: <LinkedinLogo size={32} weight="light" />,
-              href: "https://www.linkedin.com/in/francisco-sérgio-feitosa-lima-segundo-874118210/",
-              label: "LinkedIn",
-            },
-            {
-              icon: <InstagramLogo size={32} weight="light" />,
-              href: "https://www.instagram.com/sergio_ffeitosa/",
-              label: "Instagram",
-            },
-            {
-              icon: <EnvelopeSimple size={32} weight="light" />,
-              href: "mailto:fsfsegundo@hotmail.com",
-              label: "Email",
-            },
-          ].map((social, index) => (
-            <motion.div
-              key={index}
-              className="relative group"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{
-                opacity: 1,
-                y: 0,
-                transition: { delay: 1 + index * 0.1 },
-              }}
-            >
-              <div className="absolute inset-0 bg-rose-500/20 rounded-xl blur-md opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-              <div className="relative backdrop-blur-md bg-white/5 border border-white/50 rounded-xl p-1 shadow-lg transition-all duration-300 group-hover:bg-white/20 group-hover:border-rose-500/50">
-                <motion.a
-                  href={social.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label={social.label}
-                  whileHover={{
-                    scale: 1.1,
-                    color: "#f43f5e",
-                    transition: { duration: 0.2 },
-                  }}
-                  whileTap={{ scale: 0.9 }}
-                  className="text-gray-200 hover:text-rose-500 transition-colors duration-300 flex items-center justify-center"
-                >
-                  {social.icon}
-                </motion.a>
-              </div>
-            </motion.div>
-          ))}
-        </motion.div>
-        <a
-          href="https://drive.google.com/file/d/1lJkk1EkjooN6Ll1Yc9bRMDy7O_G8ZELN/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mb-20"
-        >
-          <button className="flex  text-white bg-gradient-to-bl from-rose-700/20 to-rose-800/60 p-3  font-bold hover:scale-x-102 hover:-translate-y-1 transition-all rounded-2xl ">
-            Meu Currículo <Download className="ml-2" size={24}></Download> 
-          </button>
           
-        </a>
+          <div className="md:w-2/3 flex flex-col items-center md:items-start gap-6">
+            {/* nome */}
+            <motion.h1
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              className="sm:text-5xl text-4xl font-bold text-white"
+            >
+              <motion.span
+                initial={{ x: -20, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: 0.4, duration: 0.5 }}
+                className="text-rose-500 inline-block"
+              >
+                {"<"}
+              </motion.span>
+              <span className="relative">
+                Sérgio Feitosa
+                <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-gradient-to-r from-transparent via-rose-500 to-transparent"></span>
+              </span>
+              <motion.span
+                initial={{ x: 20, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: 0.4, duration: 0.5 }}
+                className="text-rose-500 inline-block"
+              >
+                {"/>"}
+              </motion.span>
+            </motion.h1>
+
+            {/* animação do titulo */}
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.6, duration: 0.5 }}
+              className="relative self-center md:self-start"
+            >
+              <h2 className="sm:text-3xl text-2xl font-medium text-white">Desenvolvedor Web</h2>
+              
+            </motion.div>
+
+            {/* aniamçao da descricao */}
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.8, duration: 0.5 }}
+              className="sm:text-xl max-w-2xl text-center md:text-left font-light text-gray-300"
+            >
+              Graduando em{" "}
+              <span className="italic text-white font-medium relative inline-block">
+                Engenharia de Computação
+                <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-rose-500/50"></span>
+              </span>{" "}
+              na Universidade Federal do Vale do São Francisco
+            </motion.p>
+
+            {/* icones de links */}
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1, duration: 0.5 }}
+              className="flex gap-5 mt-2 justify-center md:justify-start"
+            >
+              {[
+                {
+                  icon: <GithubLogo size={32} weight="light" />,
+                  href: "https://github.com/ssergiofeitosa",
+                  label: "GitHub",
+                },
+                {
+                  icon: <LinkedinLogo size={32} weight="light" />,
+                  href: "https://www.linkedin.com/in/francisco-sérgio-feitosa-lima-segundo-874118210/",
+                  label: "LinkedIn",
+                },
+                {
+                  icon: <InstagramLogo size={32} weight="light" />,
+                  href: "https://www.instagram.com/sergio_ffeitosa/",
+                  label: "Instagram",
+                },
+                {
+                  icon: <EnvelopeSimple size={32} weight="light" />,
+                  href: "mailto:fsfsegundo@hotmail.com",
+                  label: "Email",
+                },
+              ].map((social, index) => (
+                <motion.div
+                  key={index}
+                  className="relative group"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{
+                    opacity: 1,
+                    y: 0,
+                    transition: { delay: 1 + index * 0.1 },
+                  }}
+                >
+                  <div className="absolute inset-0 bg-rose-500/20 rounded-xl blur-md opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                  <div className="relative backdrop-blur-md bg-white/5 border border-white/50 rounded-xl p-1 shadow-lg transition-all duration-300 group-hover:bg-white/20 group-hover:border-rose-500/50">
+                    <motion.a
+                      href={social.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label={social.label}
+                      whileHover={{
+                        scale: 1.1,
+                        color: "#f43f5e",
+                        transition: { duration: 0.2 },
+                      }}
+                      whileTap={{ scale: 0.9 }}
+                      className="text-gray-200 hover:text-rose-500 transition-colors duration-300 flex items-center justify-center"
+                    >
+                      {social.icon}
+                    </motion.a>
+                  </div>
+                </motion.div>
+              ))}
+            </motion.div>
+
+            {/* cvitae */}
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1.2, duration: 0.5 }}
+              className="self-center md:self-start"
+            >
+              <a
+                href="https://drive.google.com/file/d/1lJkk1EkjooN6Ll1Yc9bRMDy7O_G8ZELN/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <motion.button
+                  className="flex items-center gap-2 text-white border border-rose-500/30 bg-neutral-800/60 hover:bg-neutral-700/60 px-4 py-2 rounded-md text-sm font-medium transition-colors duration-300"
+                  whileHover={{
+                    boxShadow: "0 0 8px rgba(244, 63, 94, 0.4)",
+                    scale: 1.02,
+                  }}
+                  whileTap={{ scale: 0.98 }}
+                >
+                  <span>Meu Currículo</span>
+                  <Download size={18} className="text-rose-400" />
+                </motion.button>
+              </a>
+            </motion.div>
+          </div>
+        </div>
+
         {/* seta */}
         <motion.div
           initial={{ opacity: 0 }}
@@ -186,14 +194,14 @@ const Hero = () => {
           <motion.div
             animate={{ y: [0, 15, 0] }}
             transition={{ repeat: Number.POSITIVE_INFINITY, duration: 2.0 }}
-            className="text-rose-500"
+            className="text-rose-500 cursor-pointer"
           >
             <ArrowDown size={32} weight="bold" onClick={scrollToSection} />
           </motion.div>
         </motion.div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
